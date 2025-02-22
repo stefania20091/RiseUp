a0df1db11e95efcd94034c4d86978788e07a02ec
stefania20091 <stefaniapedko@gmail.com>
2025-02-22 11:34:24 +0200

    added part

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 const links = document.querySelectorAll('nav a');
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 4,
 
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
     links.forEach(link => {
       link.addEventListener('click', function (e) {
         links.forEach(l => l.classList.remove('active'));
